dcd65dee4c0ecd860168f29b83966c97a464274e
mitsuonitta <infinias320@gmail.com>
2023-03-02 11:40:48 +0900

    second_commit

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,8 +1,11 @@
 FactoryBot.define do
-  factory :user do
-    name {Faker::Name.last_name}
-    email {Faker::Internet.free_email}
-    password {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
+  factory :message do
+    content {Faker::Lorem.sentence}
+    association :user
+    association :room
+
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
